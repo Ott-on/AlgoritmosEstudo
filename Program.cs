fe6b2026acc24f0032e980b8ef3b237b655770a8
Ott-on <ottonvinicius1004@gmail.com>
2025-12-16 22:26:45 -0300

    adicionando novas estruturas de lista e ordenação por seleção

--- Program.cs
+++ Program.cs
@@ -1,8 +1,26 @@
 ﻿using Algoritmos_Aprendizagem.IntroducaoAlgoritmos;
+using Algoritmos_Aprendizagem.ListaEncadeada;
+using Algoritmos_Aprendizagem.OrdenacaoPorSelecao.SelectionSort;
 
 // Pesquisa Binária
-IList<int> lista = [1, 3, 5, 7, 9];
+IList<int> listaPesquisaBinaria = [1, 3, 5, 7, 9];
 
-Console.WriteLine(PesquisaBinaria.PesquisaBinariaFuncao(lista, 3)?.ToString() ?? "null");
-Console.WriteLine(PesquisaBinaria.PesquisaBinariaFuncao(lista, -1)?.ToString() ?? "null");
+Console.WriteLine(PesquisaBinaria.PesquisaBinariaFuncao(listaPesquisaBinaria, 3)?.ToString() ?? "null");
+Console.WriteLine(PesquisaBinaria.PesquisaBinariaFuncao(listaPesquisaBinaria, -1)?.ToString() ?? "null");
+
+// Lista Encadeada
+
+var listaEncadeada = new LinkedList();
+listaEncadeada.Add(2);
+listaEncadeada.Add(5);
+listaEncadeada.Add(7);
+
+Console.WriteLine($"Tamanho da lista: {listaEncadeada.Tamanho}");
+listaEncadeada.Print();
+
+// Ordenacao por Seleção
+
+IList<int> listaOrdenacaoPorSelecao = [5, 3, 6, 2, 10];
+
+Console.WriteLine($"Lista ordenada por seleção: {(string.Join(", ", SelectionSort.OrdenacaoPorSelecao(listaOrdenacaoPorSelecao)))}");
 
